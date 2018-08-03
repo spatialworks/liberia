da97f72badf5f5c8e1e7bd3dd834dca5073f8c3c
ernestguevarra <ernestgmd@gmail.com>
2018-08-03 11:14:30 +0100

    update data

---
+++ data-raw/popdata.R
@@ -0,0 +1,9 @@
+
+library(readxl)
+
+locality <- read_xlsx(path = "data-raw/popdata.xlsx", sheet = 1)
+devtools::use_data(locality, overwrite = TRUE)
+
+enumerationArea <- read_xlsx(path = "data-raw/popdata.xlsx", sheet = 2)
+devtools::use_data(enumerationArea, overwrite = TRUE)
+
