15992a6c1f87f1a3c1646447b9837f3a0978a03b
Ernest Guevarra <ernest@validmeasures.org>
2018-08-22 09:10:12 +0000

    update data

--- data-raw/popdata.R
+++ data-raw/popdata.R
@@ -7,3 +7,6 @@ devtools::use_data(locality, overwrite = TRUE)
 enumerationArea <- read_xlsx(path = "data-raw/popdata.xlsx", sheet = 2)
 devtools::use_data(enumerationArea, overwrite = TRUE)
 
+monrovia <- read_xlsx(path = "data-raw/monrovia.xlsx", sheet = 1)
+devtools::use_data(monrovia, overwrite = TRUE)
+
