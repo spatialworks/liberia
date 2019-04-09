e8d8ed799e51005994048de5d015178faa779db6
Ernest Guevarra <ernest@validmeasures.org>
2019-04-09 07:05:54 +0100

    update createHexSticker.R

--- inst/createHexSticker.R
+++ inst/createHexSticker.R
@@ -1,7 +1,7 @@
 library(hexSticker)
 
 sticker("inst/figures/liberia.png",
-        s_x = 1, s_y = 1, s_width = 0.75,
+        s_x = 1, s_y = 1, s_width = 0.7,
         package = "", p_color = "#D84942", p_y = 1.4,
         h_size = 1, h_color = "#D84942", h_fill = NA,
         filename = "man/figures/liberia.png")
