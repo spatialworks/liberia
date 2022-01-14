d8546526efdf254fb7c658c2a3a7482c6e2c365e
Ernest Guevarra <ernest@guevarra.io>
2022-01-14 06:31:24 +0000

    update README and package site

--- inst/createHexSticker.R
+++
@@ -1,7 +0,0 @@
-library(hexSticker)
-
-sticker("inst/figures/liberia.png",
-        s_x = 0.9, s_y = 1, s_width = 0.7,
-        package = "", p_color = "#D84942", p_y = 1.4,
-        h_size = 1.5, h_color = "#D84942", h_fill = NA,
-        filename = "man/figures/liberia.png")
