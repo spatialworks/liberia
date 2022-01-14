3e78385dbe0350c4ce15021801a3e2f129377a37
Ernest Guevarra <ernest@guevarra.io>
2022-01-14 05:55:03 +0000

    shift to sf package

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-<!-- README.md is generated from README.Rmd. Please edit that file -->
-liberia: Datasets for Use in Designing Surveys in Liberia <img src="man/figures/liberia.png" width="200" align="right" />
-=========================================================================================================================
 
-[![lifecycle](https://img.shields.io/badge/lifecycle-maturing-blue.svg)](https://www.tidyverse.org/lifecycle/#maturing)
-[![Travis-CI Build
-Status](https://travis-ci.org/validmeasures/liberia.svg?branch=master)](https://travis-ci.org/validmeasures/liberia)
-[![AppVeyor Build
-Status](https://ci.appveyor.com/api/projects/status/github/validmeasures/liberia?branch=master&svg=true)](https://ci.appveyor.com/project/validmeasures/liberia)
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# liberia: Datasets for Use in Designing Surveys in Liberia <img src="man/figures/logo.svg" width="200" align="right" />
+
+<!-- badges: start -->
+
+[![Project Status: Active – The project has reached a stable, usable
+state and is being actively
+developed.](https://www.repostatus.org/badges/latest/active.svg)](https://www.repostatus.org/#active)
+<!-- badges: end -->
 
 Designing surveys require relevant datasets to be used as basis for
 sample size calculations, sampling design, survey planning/logistics and
@@ -16,12 +18,11 @@ survey datasets that can be used for estimating indicator variance and
 design effects. This package contains relevant datasets for use in
 designing surveys in Liberia.
 
-Installation
-------------
+## Installation
 
-Install `liberia` package from GitHub via `devtools`:
+Install `liberia` package from GitHub via \`remotes\`\`:
 
 ``` r
-if(!require(devtools)) install.packages("devtools")
-install_github("validmeasures/liberia")
+if (!require(remotes)) install.packages("remotes")
+remotes::install_github("spatialworks/liberia")
 ```
--- data-raw/mapdata.R
+++ data-raw/mapdata.R
@@ -3,9 +3,7 @@
 # Libraries
 #
 ################################################################################
-library(rgeos)
-library(rgdal)
-library(raster)
+library(sf)
 library(readxl)
 library(stringr)
 
@@ -16,54 +14,87 @@ library(stringr)
 #
 ################################################################################
 
-counties <- readOGR(dsn = "data-raw/maps/lbr_admbnda_adm1_ocha",
-                    layer = "lbr_admbnda_adm1_ocha",
-                    verbose = FALSE)
+## Counties
+#counties <- readOGR(dsn = "data-raw/maps/lbr_admbnda_adm1_ocha",
+#                    layer = "lbr_admbnda_adm1_ocha",
+#                    verbose = FALSE)
 
-devtools::use_data(counties, overwrite = TRUE)
-
-districts <- readOGR(dsn = "data-raw/maps/lbr_admbnda_adm2_ocha",
-                     layer = "lbr_admbnda_adm2_ocha",
-                     verbose = FALSE)
-
-devtools::use_data(districts, overwrite = TRUE)
-
-clans <- readOGR(dsn = "data-raw/maps/clans",
-                    layer = "LIB",
-                    verbose = FALSE)
-
-clans <- clans[ , 1:10]
-
-devtools::use_data(clans, overwrite = TRUE)
+counties <- sf::st_read(
+  dsn = "data-raw/maps/lbr_admbnda_adm1_ocha",
+  layer = "lbr_admbnda_adm1_ocha"
+)
+usethis::use_data(counties, overwrite = TRUE)
 
 
-settlements <- readOGR(dsn = "data-raw/maps/lbr_plp_ocha",
-                       layer = "lbr_plp_ocha",
-                       verbose = FALSE)
+## Districts
+# districts <- readOGR(dsn = "data-raw/maps/lbr_admbnda_adm2_ocha",
+#                      layer = "lbr_admbnda_adm2_ocha",
+#                      verbose = FALSE)
 
-devtools::use_data(settlements, overwrite = TRUE)
+districts <- sf::st_read(
+  dsn = "data-raw/maps/lbr_admbnda_adm2_ocha",
+  layer = "lbr_admbnda_adm2_ocha"
+)
+usethis::use_data(districts, overwrite = TRUE)
 
-grandBassaEA <- readOGR(dsn = "data-raw/maps/Bassa_EA",
-                        layer = "Bassa_EA",
-                        verbose = FALSE)
+## Clans
+#clans <- readOGR(dsn = "data-raw/maps/clans",
+#                    layer = "LIB",
+#                    verbose = FALSE)
+#clans <- clans[ , 1:10]
 
-grandBassaEA <- spTransform(grandBassaEA,
-                            CRSobj = CRS(proj4string(counties)))
+clans <- sf::st_read(dsn = "data-raw/maps/clans", layer = "LIB")
+usethis::use_data(clans, overwrite = TRUE)
 
-devtools::use_data(grandBassaEA, overwrite = TRUE)
 
-greaterMonroviaEA <- readOGR(dsn = "data-raw/maps/Greater_Monrovia_EA",
-                             layer = "Greater_Monrovia_EA",
-                             verbose = FALSE)
+## Settlements
+#settlements <- readOGR(dsn = "data-raw/maps/lbr_plp_ocha",
+#                       layer = "lbr_plp_ocha",
+#                       verbose = FALSE)
 
-greaterMonroviaEA <- spTransform(greaterMonroviaEA,
-                                 CRSobj = CRS(proj4string(counties)))
+settlements <- sf::st_read(
+  dsn = "data-raw/maps/lbr_plp_ocha", layer = "lbr_plp_ocha"
+)
+usethis::use_data(settlements, overwrite = TRUE)
 
-devtools::use_data(greaterMonroviaEA, overwrite = TRUE)
+## Enumeration areas
+# grandBassaEA <- readOGR(dsn = "data-raw/maps/Bassa_EA",
+#                         layer = "Bassa_EA",
+#                         verbose = FALSE)
+#
+# grandBassaEA <- spTransform(grandBassaEA,
+#                             CRSobj = CRS(proj4string(counties)))
+#
+# devtools::use_data(grandBassaEA, overwrite = TRUE)
 
-health_facilities <- read_excel(path = "data-raw/maps/141121-lr-health-care-facilities.xlsx", sheet = 2, skip = 1)
+grandBassaEA <- sf::st_read(
+  dsn = "data-raw/maps/ea", layer = "grand_bassa_ruralEAs"
+)
+sf::st_crs(grandBassaEA) <- 4326
+usethis::use_data(grandBassaEA, overwrite = TRUE)
 
-names(health_facilities) <- str_remove(string = names(health_facilities), pattern = "#")
+# greaterMonroviaEA <- readOGR(dsn = "data-raw/maps/Greater_Monrovia_EA",
+#                              layer = "Greater_Monrovia_EA",
+#                              verbose = FALSE)
+#
+# greaterMonroviaEA <- spTransform(greaterMonroviaEA,
+#                                  CRSobj = CRS(proj4string(counties)))
+#
+# devtools::use_data(greaterMonroviaEA, overwrite = TRUE)
+
+greaterMonroviaEA <- sf::st_read(
+  dsn = "data-raw/maps/ea", layer = "greater_monrovia_eas"
+)
+sf::st_crs(greaterMonroviaEA) <- 4326
+usethis::use_data(greaterMonroviaEA,  overwrite = TRUE)
+
+health_facilities <- read_excel(
+  path = "data-raw/maps/141121-lr-health-care-facilities.xlsx",
+  sheet = 2, skip = 1
+)
+names(health_facilities) <- str_remove(
+  string = names(health_facilities), pattern = "#"
+)
 
 
 
